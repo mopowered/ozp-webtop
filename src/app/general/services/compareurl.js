09acee976a36505a0748ee67833cc3a1bf58bd74
Alan Ward <arw180@gmail.com>
2014-09-04 15:33:34 -0400

    Updates

--- src/app/general/services/compareurl.js
+++ src/app/general/services/compareurl.js
@@ -25,13 +25,13 @@ angular.module('ozpWebtopApp.general')
     }
 
     // TODO DEBUG FOR DEMO PURPOSES ONLY CHANGE THIS!!!
-    return false;
+    //return false;
 
     // TODO: PUT THIS BACK IN AFTER DEMO
     // If all comparisons are true, the url represents the same origin
-//    return a.hostname === loc.hostname &&
-//      a.port === loc.port &&
-//      a.protocol === loc.protocol;
+    return a.hostname === loc.hostname &&
+      a.port === loc.port &&
+      a.protocol === loc.protocol;
   };
 
   /**
